f002c529dc381b24f3383eca360a11b784b4fa2b
이윤지 <82319291+earlyoonj@users.noreply.github.com>
2022-12-23 13:36:01 +0900

    feat: setup next project

---
+++ pages/index.tsx
@@ -0,0 +1,20 @@
+import Head from 'next/head';
+
+export default function ShadowingBudget() {
+    return (
+        <>
+            <Head>
+                <title>Shadowing Budget</title>
+                <meta
+                    name="description"
+                    content="Budget planner and expense tracker"
+                />
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1"
+                />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+        </>
+    );
+}
