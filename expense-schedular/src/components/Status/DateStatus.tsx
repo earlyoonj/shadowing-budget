4be3469f3b96c9baf463dac208a1ea2ec0359b26
이윤지 <82319291+earlyoonj@users.noreply.github.com>
2022-12-27 20:32:51 +0900

    feat: Status / create DateStatus & Calendar component

---
+++ expense-schedular/src/components/Status/DateStatus.tsx
@@ -0,0 +1,19 @@
+import Calendar from '../UI/Calendar';
+
+function DateStatus() {
+    const clickHandler = (event: React.MouseEvent) => {
+        const date = (event.target as HTMLTableDataCellElement).getAttribute(
+            'data-date'
+        );
+        console.log(date);
+    };
+    return (
+        <Calendar
+            startDate={new Date(2022, 11, 1)}
+            endDate={new Date(2022, 11, 31)}
+            onClick={clickHandler}
+        ></Calendar>
+    );
+}
+
+export default DateStatus;
